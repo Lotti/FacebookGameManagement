89f0994e6ffb8419e8dfe58f796b33db31ff9283
Lotti <lotti86@gmail.com>
2014-05-27 10:47:48 +0200

    removed unused code

--- js/app.js
+++ js/app.js
@@ -23,10 +23,3 @@ app.config(['$routeProvider',
         	redirectTo: '/'
       	});
 }]);
-
-/*
-app.config(['$httpProvider', function($httpProvider) {
-	$httpProvider.defaults.useXDomain = true;
-	delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]);
-*/
